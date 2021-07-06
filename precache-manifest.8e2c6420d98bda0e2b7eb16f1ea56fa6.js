4753d0044ba61ef653bf1d5600ae3c987ee91332
Runal <bandishtirunal@gmail.com>
2021-07-06 21:00:50 +0530

    first commit

---
+++ precache-manifest.8e2c6420d98bda0e2b7eb16f1ea56fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4e05d5af89e32a241bf",
+    "url": "/static/css/main.e3d0ba7f.chunk.css"
+  },
+  {
+    "revision": "d4e05d5af89e32a241bf",
+    "url": "/static/js/main.d4e05d5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "da76d4b5ea12ad96d1f4",
+    "url": "/static/js/2.da76d4b5.chunk.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/static/media/logo.12a6659f.svg"
+  },
+  {
+    "revision": "f78d9310040d0d45f1df1b0601f91b77",
+    "url": "/index.html"
+  }
+];
